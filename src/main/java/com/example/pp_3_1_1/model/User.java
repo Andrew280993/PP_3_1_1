b81aa3faeb3f63489fe3996656318c1ba485cc70
Andrew280993 <97826938+Andrew280993@users.noreply.github.com>
2022-12-01 23:20:34 +0300

    new-1 (#1)

--- src/main/java/com/example/pp_3_1_1/model/User.java
+++ src/main/java/com/example/pp_3_1_1/model/User.java
@@ -11,7 +11,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column
     @NotEmpty(message = "Name should not be empty")
     private String name;
